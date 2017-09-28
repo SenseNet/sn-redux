7110dfb1abfd3e7db402f58c21ca0b9b19e8bc0f
Aniko Litvanyi <herflis33@gmail.com>
2017-09-28 13:26:12 +0200

    [KFI]test(Reducers): Add test for checking the entity list after update success

--- test/ReducersTests.ts
+++ test/ReducersTests.ts
@@ -171,6 +171,34 @@ describe('Reducers', () => {
             expect(Reducers.entities({}, { response: { entities: { entities: { a: 0, b: 2 } } } }))
                 .to.be.deep.eq({ a: 0, b: 2 });
         });
+        it('should handle UPDATE_CONTENT_SUCCESS', () => {
+            const entities = {
+                5145: {
+                    Id: 5145,
+                    DisplayName: 'Some Article',
+                    Status: ['Active']
+                },
+                5146: {
+                    Id: 5146,
+                    Displayname: 'Other Article',
+                    Status: ['Completed']
+                }
+            };
+            expect(Reducers.entities(entities, { type: 'UPDATE_CONTENT_SUCCESS', response: { Id: 5145, DisplayName: 'aaa', Status: ['Active'] } })).to.be.deep.equal(
+                {
+                    5145: {
+                        Id: 5145,
+                        DisplayName: 'aaa',
+                        Status: ['Active']
+                    },
+                    5146: {
+                        Id: 5146,
+                        Displayname: 'Other Article',
+                        Status: ['Completed']
+                    }
+                }
+            );
+        });
     });
 
     describe('isFetching reducer', () => {
