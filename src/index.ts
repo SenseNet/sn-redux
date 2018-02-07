2240959c4c80a7890edb730c234ca14f1ff465ac
Aniko Litvanyi <herflis33@gmail.com>
2018-02-07 15:31:21 +0100

    [KFI]build: Modify main file names from sn-redux.* to index.* and cleanup the build process

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-import './Actions';
-import './Epics';
-import './Reducers';
-import './Schema';
-import './Store';
-
-export { Actions } from './Actions';
+export { Actions } from  './Actions';
 export { Epics } from './Epics';
 export { Reducers } from './Reducers';
 export { Schemas } from './Schema';
