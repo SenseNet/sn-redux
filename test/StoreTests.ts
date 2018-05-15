c9a75382a93ae25521993615b56d143c963077db
Gallay Lajos <gallay.lajos@live.com>
2018-05-15 10:52:37 +0200

    [KFI]test(store): added unit tests to verify if default store value and preloaded state is applied w

--- test/StoreTests.ts
+++ test/StoreTests.ts
@@ -1,8 +1,8 @@
 import { Repository } from '@sensenet/client-core'
 import * as Chai from 'chai'
-import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { combineReducers, Reducer } from 'redux'
 import { createLogger } from 'redux-logger'
-import * as Store from '../src/Store'
+import { createSensenetStore } from '../src/Store'
 const expect = Chai.expect
 
 describe('Store', () => {
@@ -10,25 +10,37 @@ describe('Store', () => {
     const loggerMiddleware = createLogger()
     const middlewareArray = []
     middlewareArray.push(loggerMiddleware)
-    const testReducer = (state = {}, action) => {
+
+    const testReducer: Reducer<{testValue: number}> = (state = {testValue: 1}, action) => {
         return state
     }
-    const myReducer = combineReducers({ testReducer })
-    const store = createStore(
-        myReducer,
-        {},
-        applyMiddleware(...middlewareArray),
-    )
+
+    const rootReducer = combineReducers<{testReducer: {testValue: number}}>({
+        testReducer,
+    })
+
     it('should return a redux store', () => {
-        expect(typeof Store.createSensenetStore({ repository, rootReducer: myReducer })).to.be.equal(typeof store)
+        expect(createSensenetStore({ repository, rootReducer })).to.be.instanceof(Object)
     })
     it('should return a redux store', () => {
-        expect(typeof Store.createSensenetStore({ repository, rootReducer: myReducer, middlewares: null, persistedState: {} })).to.be.equal(typeof store)
+        expect(createSensenetStore({ repository, rootReducer, middlewares: null, persistedState: {testReducer: {testValue: 3} }})).to.be.instanceof(Object)
     })
     it('should return a redux store', () => {
-        expect(typeof Store.createSensenetStore({ repository, rootReducer: myReducer, middlewares: null })).to.be.equal(typeof store)
+        expect(createSensenetStore({ repository, rootReducer, middlewares: null })).to.be.instanceof(Object)
     })
     it('should return a redux store', () => {
-        expect(typeof Store.createSensenetStore({ repository, rootReducer: myReducer, middlewares: middlewareArray })).to.be.equal(typeof store)
+        expect(createSensenetStore({ repository, rootReducer, middlewares: middlewareArray })).to.be.instanceof(Object)
+    })
+
+    it('default state should be applied', () => {
+        const tempStore = createSensenetStore({ repository, rootReducer, middlewares: middlewareArray })
+        const state = tempStore.getState()
+        expect(state.testReducer.testValue).to.be.eq(1)
+    })
+
+    it('persisted state should be applied', () => {
+        const tempStore = createSensenetStore({ repository, rootReducer, middlewares: middlewareArray, persistedState: {testReducer: {testValue: 3} }})
+        const state = tempStore.getState()
+        expect(state.testReducer.testValue).to.be.eq(3)
     })
 })
